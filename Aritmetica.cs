de64ffb3b494100b186aafced29bd0ec5bd09c45
garzina <146721011+garzina@users.noreply.github.com>
2023-10-15 20:09:22 +0100

    Adicionar metodo Converter

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -5,31 +5,52 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Calculos
-{    
-        /// <summary>
-        /// Implementa operações aritméticas.
-        /// </summary>
-        internal class Aritmetica
-        {
-            public int Somar(int x, int y)
+{
+
+    /// <summary>
+    /// Tipo de conversão de temperatura a executar.
+    /// </summary>
+    public enum ConversaoTemperatura
+    {
+        Nulo = 0,
+        CelsiusFahrenheit,
+        FahrenheitCelsius
+    }
+
+
+    /// <summary>
+    /// Implementa operações aritméticas.
+    /// </summary>
+    internal class Aritmetica
+    {
+        
+         /// Operação soma.
+         /// <returns>Retorna a soma de dois números.</returns>
+        public int Somar(int x, int y)
             {
                 return x + y;
             }
-            public int Subtrair(int x, int y)
+        /// Operação subtração.
+        /// <returns>Retorna o resultado da subtração de dois números.</returns>
+        public int Subtrair(int x, int y)
             {
                 return x - y;
             }
-        /// <summary>
-        /// Operação soma.
-        /// </summary>
-        /// <returns>Retorna a soma de dois números.</returns>
+        
+                
+        public double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+        {
+            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
+            {
+                return (temperatura * 1.8000 + 32);
+            }
+            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
+            {
+                return ((temperatura - 32) / 1.8000);
+            }
 
-        /// <summary>
-        /// Operação subtração.
-        /// </summary>
-        /// <returns>Retorna o resultado da subtração de dois números.</returns>
-
-
+            return -1;
+        }
 
     }
 
